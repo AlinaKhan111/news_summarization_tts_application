52e2565aad5cf9cea4260849d6b570920119db05
Alina Khan <khanalinaa000@gmail.com>
2025-03-24 14:07:31 +0530

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
 echo "🚀 Starting FastAPI Backend..."
 uvicorn src.api:app --host 0.0.0.0 --port 8000 --reload &
 sleep 5
@@ -10,4 +8,3 @@ echo "✅ FastAPI Backend is running!"
 echo "📚 Starting Streamlit Frontend..."
 streamlit run src/app.py
 echo "✅ Streamlit is running!"
-
